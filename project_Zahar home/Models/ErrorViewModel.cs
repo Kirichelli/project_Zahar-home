4567ad2e9126367ab2d13b120c84c20d6fcdfc4b
Kirichelli <hlebosolow.kirill@yandex.ru>
2022-04-27 16:16:31 +0300

    Добавьте файлы проекта.

---
+++ project_Zahar home/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace project_Zahar_home.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
