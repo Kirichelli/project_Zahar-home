19b06c5136e9606838e39ce8120b0ed8aa19f4e2
Kirichelli <hlebosolow.kirill@yandex.ru>
2022-05-21 17:22:57 +0300

    Добавил оставшиеся таблицы в логику и добавил интерфейс-шаблоны

---
+++ project_Zahar_home.Logic/Rating_users/IRating_user.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_Zahar_home.Logic.Rating_users
+{
+    internal interface IRating_user
+    {
+    }
+}
