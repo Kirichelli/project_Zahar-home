7256a388978e5b18526c7f79ba7b10cef1cfebb5
bonNope <85677635+bonNope@users.noreply.github.com>
2022-05-31 17:57:11 +0300

    добавил атрибут валидации для проверки правильности ввода email

--- project_Zahar home/Models/RegisterModel.cs
+++ project_Zahar home/Models/RegisterModel.cs
@@ -4,7 +4,8 @@ namespace project_Zahar_home.Models
 {
     public class RegisterModel
     {
-            [Required(ErrorMessage = "Не указан Email")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный адрес")]
+        [Required(ErrorMessage = "Не указан Email")]
             public string Email { get; set; }
 
             [Required(ErrorMessage = "Не указано имя пользователя")]
